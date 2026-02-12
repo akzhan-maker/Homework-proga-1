7d02b19a9c742d92fb147d2f831a463b63ede394
Akzhan <ergalievaakzhan@gmail.com>
2026-02-12 11:11:50 +0500

    1-15 esep

--- Practice/practise.py
+++ Practice/practise.py
@@ -1,15 +1,3 @@
-import csv
-with open("names.csv", "w", newline="", encoding="utf-8") as f1:
-    writer = csv.writer(f1)
-    data = input( )
-    names = data.split()
-    for name in names:
-        writer.writerow([name.capitalize()])
-print("-" * 20)
-print( )
-with open("names.csv", "r", encoding="utf-8") as f1:
-    reader = csv.reader(f1)
-    for row in reader:
-        print(row[0])
+
 
 
