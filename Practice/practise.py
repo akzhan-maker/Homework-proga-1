8287ee52ae4e638e1283553943412f18bc53f21d
Akzhan <ergalievaakzhan@gmail.com>
2026-02-17 09:52:10 +0500

    changes

--- Practice/practise.py
+++ Practice/practise.py
@@ -30,16 +30,6 @@ with open("names-upper", "r", encoding="utf-8") as f1:
 
 #4
 import csv
-with open("new_file", "w", newline="", encoding="utf-8") as f1:
-    f1.write("akzhan\n")
-    f1.write("python")
-
-with open("new_file", "r", encoding="utf-8") as f1:
-    content = f1.read()
-    print(content)
-
-#5
-import csv
 
 with open("new_file.csv", "w", newline="", encoding="utf-8") as f1:
     writer = csv.writer(f1)
