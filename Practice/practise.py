84ae935124bf6ae135bc1b8c866fb048910515c7
Akzhan <ergalievaakzhan@gmail.com>
2026-02-17 09:25:25 +0500

    practice 2

--- Practice/practise.py
+++ Practice/practise.py
@@ -7,4 +7,13 @@ with open("new_file", "r", encoding="utf-8") as f1:
     content = f1.read()
     print(content)
 
+#2
+with open("data_file", "w", newline="", encoding="utf-8") as f1:
+    for i in range(1,10):
+        f1.write(str(i) + "\n")
+
+with open("data_file", "r", encoding="utf-8") as f1:
+    content = f1.read()
+    print(content)
+
 
