e85c3d5ddb2428bf557612a36050f19d05d875fd
Akzhan <ergalievaakzhan@gmail.com>
2026-02-17 09:50:59 +0500

    5

--- Practice/practise.py
+++ Practice/practise.py
@@ -28,7 +28,27 @@ with open("names-upper", "r", encoding="utf-8") as f1:
     for line in f1:
         print(line.strip().upper())
 
+#4
+import csv
+with open("new_file", "w", newline="", encoding="utf-8") as f1:
+    f1.write("akzhan\n")
+    f1.write("python")
 
+with open("new_file", "r", encoding="utf-8") as f1:
+    content = f1.read()
+    print(content)
 
+#5
+import csv
+
+with open("new_file.csv", "w", newline="", encoding="utf-8") as f1:
+    writer = csv.writer(f1)
+    writer.writerow(["akzhan"])
+    writer.writerow(["python"])
+
+with open("new_file.csv", "r", encoding="utf-8") as f1:
+    reader = csv.reader(f1)
+    for row in reader:
+        print(row[0])
 
 
