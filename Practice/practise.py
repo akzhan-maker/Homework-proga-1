d27eb8056b73ec3ec9f509e9742a65d3f6634f66
Akzhan <ergalievaakzhan@gmail.com>
2026-02-17 09:17:01 +0500

    practice1

--- Practice/practise.py
+++ Practice/practise.py
@@ -1,3 +1,10 @@
+#1
+with open("new_file", "w", newline="", encoding="utf-8") as f1:
+    f1.write("akzhan\n")
+    f1.write("python")
 
+with open("new_file", "r", encoding="utf-8") as f1:
+    content = f1.read()
+    print(content)
 
 
