f393128ec91e011e404bf5cd9446a34c7e3191e5
Akzhan <ergalievaakzhan@gmail.com>
2026-04-08 21:17:31 +0500

    Добавлена лабораторная 4

---
+++ lab_4/4.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flasgger import Swagger
+
+app = Flask(__name__)
+swagger = Swagger(app)
+
+def player():
+
+
+
+
+if name == '__main__':
+    app.run(port=5000)
