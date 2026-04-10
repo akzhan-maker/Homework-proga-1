5e6e112f9a0e24185ef8f363bb3935d81baebe9d
Akzhan <ergalievaakzhan@gmail.com>
2026-04-10 22:25:56 +0500

    4 esep

--- lab 5/lab 5.py
+++ lab 5/lab 5.py
@@ -50,7 +50,6 @@ class User:
 
 u = User.from_string("2, Alice Wonderland , alice@wonder.com")
 print(u)
-'''
 #3
 class Product:
     def __init__(self, product_id: int, name: str, price: float, category: str):
@@ -82,7 +81,53 @@ p1 = Product(1, "Laptop", 1200.0, "Electronics")
 p2 = Product(1, "Laptop", 1200.0, "Electronics")
 p3 = Product(2, "Mouse", 25.0, "Electronics")
 
-# Проверка to_dict
 print(f"Словарь: {p1.to_dict()}")
 product_set = {p1, p2, p3}
 print(f"Количество уникальных товаров: {len(product_set)}")
+
+#4
+class Product:
+    def __init__(self, product_id, name, price, category):
+        self.id = product_id
+        self.name = name
+        self.price = float(price)
+        self.category = category
+
+    def __eq__(self, other):
+        if not isinstance(other, Product):
+            return False
+        return self.id == other.id
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __str__(self):
+        return f"Product(id={self.id}, name='{self.name}')"
+
+
+class Inventory:
+    def __init__(self):
+        self.products = []
+
+    def add_product(self, product):
+        if any(p.id == product.id for p in self.products):
+            return
+        self.products.append(product)
+
+    def get_all_products(self):
+        return self.products
+
+    def to_dict(self):
+        return {p.id: p for p in self.products}
+
+
+inv = Inventory()
+p1 = Product(1, "Laptop", 1200.0, "Electronics")
+p2 = Product(2, "Mouse", 25.0, "Electronics")
+
+inv.add_product(p1)
+inv.add_product(p2)
+
+print(f"Всего товаров: {len(inv.get_all_products())}")
+print(f"Словарь инвентаря: {inv.to_dict()}")
+'''
